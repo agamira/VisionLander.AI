e21d270902fa1f42ceff8bb1542970788af10852
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-27 20:12:22 +0400

    app config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./assets/sass/main.scss";
+import { Home, Root } from "./pages";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <main id="home">
+
+    </main>
+  );
+};
+
+export { Home };
