467e660b5e4323e6a08466dce5aa606b5e3fe35c
Jeyhun Abilov <abilovv599@gmail.com>
2023-09-28 18:17:51 +0400

    add tags for form

--- src/pages/components/GeneratorFormSection/GeneratorFormSection.jsx
+++ src/pages/components/GeneratorFormSection/GeneratorFormSection.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./GeneratorFormSection.scss";
 import { Button } from "../../../components";
 import configIcon from "../../../assets/icon/config.svg";
@@ -6,19 +7,25 @@ import hardwell from "../../../assets/img/hardwell-template.png";
 import axios from "axios";
 
 const GeneratorFormSection = () => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const prompt = e.target.elements.prompt.value;
     const templateId = +e.target.elements.templateId.value;
     const formData = { prompt, templateId };
 
+    console.log(formData);
+
     axios
-        .post("http://localhost:3000/submit_form", formData, { withCredentials: true })
-        .then((response) => {
-          if (response.status === 200) {
-            window.location.href = "http://localhost:3000/redactor";
-          }
-        })
+      .post("http://localhost:3000/submit_form", formData, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          window.location.href = "http://localhost:3000/redactor";
+        }
+      });
   };
 
   return (
@@ -33,18 +40,61 @@ const GeneratorFormSection = () => {
                     type="text"
                     placeholder="Describe What you want:"
                     name="prompt"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
                   />
                   <Button className="btn--primary" type="submit">
                     Generate
                   </Button>
                 </div>
-                <Button className="config-btn">
+                <Button className="config-btn" type={"button"}>
                   <img src={configIcon} alt="" />
                 </Button>
               </div>
               <div className="tags-group">
                 <span>Popular Tags:</span>
-                <div className="tags-list">{/* <Button></Button> */}</div>
+                <div className="tags-list">
+                  <Button
+                    onClick={(e) => setInputValue(e.target.value)}
+                    value={"Landing"}
+                    className="btn--secondary"
+                    type={"button"}
+                  >
+                    Landing
+                  </Button>
+                  <Button
+                    onClick={(e) => setInputValue(e.target.value)}
+                    value={"E Commerce"}
+                    className="btn--secondary"
+                    type={"button"}
+                  >
+                    E Commerce
+                  </Button>
+                  <Button
+                    onClick={(e) => setInputValue(e.target.value)}
+                    value={"Portfolio"}
+                    className="btn--secondary"
+                    type={"button"}
+                  >
+                    Portfolio
+                  </Button>
+                  <Button
+                    onClick={(e) => setInputValue(e.target.value)}
+                    value={"Art works"}
+                    className="btn--secondary"
+                    type={"button"}
+                  >
+                    Art works
+                  </Button>
+                  <Button
+                    onClick={(e) => setInputValue(e.target.value)}
+                    value={"Creative"}
+                    className="btn--secondary"
+                    type={"button"}
+                  >
+                    Creative
+                  </Button>
+                </div>
               </div>
             </div>
             <div className="builder-templates">
