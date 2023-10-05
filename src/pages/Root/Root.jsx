de19b2cfb479d816f8fef2a2ae38caade080fea0
Jeyhun Abilov <abilovv599@gmail.com>
2023-10-05 22:07:24 +0400

    bug fix

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -37,7 +37,7 @@ const Root = () => {
   const [loggedUser, setLoggedUser] = useState(null);
 
   useEffect(() => {
-    auth
+    auth()
       .then((res) => {
         if (res.status === 200) {
           setIsLoggedIn(true);
