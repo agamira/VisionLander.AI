f2497880ae53af5c438b3f62e27c8e173bac4252
Jeyhun Abilov <abilovv599@gmail.com>
2023-10-22 21:42:08 +0400

    remove unused components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { Logo } from "./Logo/Logo";
 export { Button } from "./Button/Button";
 export { Image } from "./Image/Image";
-export { VideoPlayer } from "./VideoPlayer/VideoPlayer";
 export { StepsCard } from "./StepsCard/StepsCard";
 export { Modal } from "./Modal/Modal";
 export { RegisterForm } from "./RegisterForm/RegisterForm";
