c470134cda46aabfa92c8ff2b15047385c07b7c9
Jeyhun Abilov <abilovv599@gmail.com>
2023-11-08 00:57:46 +0400

    Remove loading component and simplify return statement in PrivateRoutes.jsx

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,9 +2,6 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { authAsync } from "../redux/authSlice";
-import { Button, Result } from "antd";
-import { Loading } from "../components";
-import loadingIcon from "../assets/icon/loading.svg";
 
 const PrivateRoutes = () => {
   const navigate = useNavigate();
@@ -15,23 +12,7 @@ const PrivateRoutes = () => {
   const isLoading = useSelector((state) => state.auth.isLoading);
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
-  return isLoading ? (
-    <Loading loadingIcon={loadingIcon} loadingMessage={"Loading redactor..."} />
-  ) : isAuthenticated ? (
-    <Outlet />
-  ) : (
-    <Result
-      style={{ display: "flex", flexDirection: "column", height: "80vh" }}
-      status="403"
-      title="403"
-      subTitle="Sorry, you are not authorized to access this page."
-      extra={
-        <Button onClick={() => navigate("/")} type="primary">
-          Back Home
-        </Button>
-      }
-    />
-  );
+  return isLoading ? null : isAuthenticated ? <Outlet /> : navigate("/");
 };
 
 export { PrivateRoutes };
