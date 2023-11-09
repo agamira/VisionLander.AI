dc9d3f53fa13568ac48b38935f48caa5b9035682
Jeyhun Abilov <abilovv599@gmail.com>
2023-11-10 01:04:33 +0400

    Add buttons and styles to SiteCard component

--- src/components/SiteCard/SiteCard.jsx
+++ src/components/SiteCard/SiteCard.jsx
@@ -1,11 +1,37 @@
+import { Button } from "..";
 import "./SiteCard.scss";
 
 const SiteCard = ({ siteImage }) => {
   return (
     <div className="site-card">
       <div className="site-info">
-        <p className="site-name">Site Name</p>
-        <p className="site-url">Site URL</p>
+        <div className="site-name">
+          <p>Site Name</p>
+          <Button onClick={() => console.log("clicked")}></Button>
+        </div>
+        <a className="site-url">Site URL</a>
+        <div className="edit-buttons">
+          <Button
+            className="btn btn--outline"
+            onClick={() => console.log("clicked")}
+          >
+            Change domain
+          </Button>
+          <Button className="btn" onClick={() => console.log("clicked")}>
+            Edit
+          </Button>
+        </div>
+        <div className="activation-buttons">
+          <Button
+            className="btn"
+            onClick={() => console.log("clicked")}
+          >
+            Activate
+          </Button>
+          <Button className="btn" onClick={() => console.log("clicked")}>
+            Publish
+          </Button>
+        </div>
       </div>
       <div className="site-img">
         <img src={siteImage} alt="website" />
