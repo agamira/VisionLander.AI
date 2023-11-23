17e22f9c8626dafb929512301597e1eeeee01a0f
Jeyhun Abilov <abilovv599@gmail.com>
2023-11-23 07:28:19 +0400

    Update subdomain validation regex pattern

--- src/components/ModalManager/ModalManager.jsx
+++ src/components/ModalManager/ModalManager.jsx
@@ -73,7 +73,8 @@ const ModalManager = ({ children }) => {
     if (!(inputValue || input2Value)) return warning("Please fill the field!");
     if (activeTab === "freeDomain") {
       // Regular expression pattern for subdomain validation
-      const subdomainRegex = /^[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/;
+      const subdomainRegex =
+        /^([a-zA-Z0-9]([-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)?([a-zA-Z0-9]{1,2}([-a-zA-Z0-9]{0,252}[a-zA-Z0-9])?)\.([a-zA-Z]{2,63})$/;
 
       // Validate subdomain using the regex pattern
       if (!subdomainRegex.test(inputValue)) {
@@ -100,7 +101,8 @@ const ModalManager = ({ children }) => {
     }
     if (activeTab === "customDomain") {
       // Regular expression pattern for subdomain validation
-      const subdomainRegex = /^[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/;
+      const subdomainRegex =
+        /^([a-zA-Z0-9]([-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)?([a-zA-Z0-9]{1,2}([-a-zA-Z0-9]{0,252}[a-zA-Z0-9])?)\.([a-zA-Z]{2,63})$/;
 
       // Validate subdomain using the regex pattern
       if (!subdomainRegex.test(input2Value)) {
